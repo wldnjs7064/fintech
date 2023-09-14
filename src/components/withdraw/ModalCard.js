33049523c9fa982748e0c022627b9344fe8a4be8
wldnjs7064 <wldnjs7064@naver.com>
2023-09-14 15:32:14 +0900

    Qr 결제 모달창 생성

---
+++ src/components/withdraw/ModalCard.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const ModalCardBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0.5rem;
+  padding: 20px;
+  border: 1px #112211 solid;
+`;
+const CardTitle = styled.div`
+  font-size: 1rem;
+  color: black;
+`;
+const FintechUseNo = styled.div`
+  font-size: 0.7rem;
+  margin-bottom: 30px;
+`;
+
+const WithDrawButton = styled.button`
+  border: none;
+  padding: 0.3rem;
+  background: #2aa450;
+  color: white;
+  margin-top: 0.3rem;
+`;
+
+const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
+  //fintechUseNo : 내계좌
+  //tofintechno : QR 코드로 읽어온 핀테크 계좌
+  const [amount, setamount] = useState("");
+
+  const genTransId = () => {
+    let countnum = Math.floor(Math.random() * 1000000000) + 1;
+    const clientNo = "M202300440";
+    let transId = clientNo + "U" + countnum;
+    return transId;
+  };
+
+  //출금 이체 기능 작성
+  //A0000 일때 alert("출금 완료")
+  const handlePayButtonClick = () => {};
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    console.log(value);
+    setamount(value);
+  };
+
+  return (
+    <ModalCardBlock>
+      <CardTitle>{bankName}</CardTitle>
+      <FintechUseNo>{fintechUseNo}</FintechUseNo>
+      <p>{tofintechno}로 돈을 보냅니다.</p>
+      <input onChange={handleChange}></input>
+      <WithDrawButton onClick={handlePayButtonClick}>결제하기</WithDrawButton>
+    </ModalCardBlock>
+  );
+};
+
+export default ModalCard;
