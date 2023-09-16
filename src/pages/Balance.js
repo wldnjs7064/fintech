2e1b066bf2cdbf86e8972fbbc23c604427da2ffc
wldnjs7064 <wldnjs7064@naver.com>
2023-09-16 22:13:05 +0900

    잔액조회 서버 연결'

--- src/pages/Balance.js
+++ src/pages/Balance.js
@@ -18,18 +18,15 @@ const Balance = () => {
     getTransactionList();
   }, []);
   const getBalance = () => {
-    const accessToken = localStorage.getItem("accessToken");
+    const ourToken = localStorage.getItem("ourtoken");
     let requestOption = {
-      url: "/v2.0/account/balance/fin_num",
+      url: "/balance",
       method: "GET",
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        Authorization: `Bearer ${accessToken}`,
+        ourtoken: ourToken,
       },
       params: {
-        bank_tran_id: genTrasId(),
-        fintech_use_num: fintechNo,
-        tran_dtime: "20230914103600",
+        fintechUseNo: fintechNo,
       },
     };
 
@@ -40,29 +37,28 @@ const Balance = () => {
   };
 
   const getTransactionList = () => {
-    const accessToken = localStorage.getItem("accessToken");
-    let requestOption = {
-      url: "/v2.0/account/transaction_list/fin_num",
-      method: "GET",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        Authorization: `Bearer ${accessToken}`,
-      },
-      params: {
-        bank_tran_id: genTrasId(),
-        fintech_use_num: fintechNo,
-        inquiry_type: "A",
-        inquiry_base: "D",
-        from_date: "20230912",
-        to_date: "20230914",
-        sort_order: "A",
-        tran_dtime: "20230914103600",
-      },
-    };
-
-    axios(requestOption).then(({ data }) => {
-      setTrasactionList(data.res_list);
-    });
+    // const accessToken = localStorage.getItem("accessToken");
+    // let requestOption = {
+    //   url: "/v2.0/account/transaction_list/fin_num",
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    //     Authorization: `Bearer ${accessToken}`,
+    //   },
+    //   params: {
+    //     bank_tran_id: genTrasId(),
+    //     fintech_use_num: fintechNo,
+    //     inquiry_type: "A",
+    //     inquiry_base: "D",
+    //     from_date: "20230912",
+    //     to_date: "20230914",
+    //     sort_order: "A",
+    //     tran_dtime: "20230914103600",
+    //   },
+    // };
+    // axios(requestOption).then(({ data }) => {
+    //   setTrasactionList(data.res_list);
+    // });
   };
 
   function generateRandom9DigitNumber() {
